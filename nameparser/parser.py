42292eb4b9a026afbb81f47b647a03a079b71f39
Derek Gulbranson <derek73@gmail.com>
2022-02-07 19:26:13 -0800

    don't test is_title() if there's a first name

--- nameparser/parser.py
+++ nameparser/parser.py
@@ -641,9 +641,9 @@ class HumanName(object):
                     except IndexError:
                         nxt = None
 
-                    if self.is_title(piece) \
+                    if not self.first \
                             and (nxt or len(pieces) == 1) \
-                            and not self.first:
+                            and self.is_title(piece):
                         self.title_list.append(piece)
                         continue
                     if not self.first:
