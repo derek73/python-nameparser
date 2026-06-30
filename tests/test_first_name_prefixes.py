b8feba4e38970beac275257776b4c82710d1f366
Derek Gulbranson <derek73@gmail.com>
2026-06-30 01:35:34 -0700

    test: remove test_default_set_contents (parsing tests cover membership)

--- tests/test_first_name_prefixes.py
+++ tests/test_first_name_prefixes.py
@@ -1,14 +1,8 @@
 from nameparser import HumanName
-from nameparser.config import CONSTANTS
-
 from tests.base import HumanNameTestBase
 
 
 class FirstNamePrefixesTestCase(HumanNameTestBase):
-
-    def test_default_set_contents(self) -> None:
-        for word in ("abdul", "abdel", "abdal", "abu", "abou", "umm"):
-            assert word in CONSTANTS.first_name_prefixes, f"{word!r} missing from first_name_prefixes"
 
     def test_is_first_name_prefix_true(self) -> None:
         hn = HumanName("test")
